2252bd3006ca3a659d9705aaf6ef4162bbe1152f
Matt Fenwick <mfenwick100@gmail.com>
2012-10-07 18:11:15 -0400

    'select' combinators: remove those with non-sensical types, replace with good ones

--- Select.hs
+++ Select.hs
@@ -1,5 +1,10 @@
 module Select (
 
+    extendAgg
+
+  , select
+  , selectMany
+
 ) where
 
 
@@ -12,26 +17,23 @@ extendAgg :: (Eq a, Eq b) => ([a] -> b) -> [a] -> [(a, b)]
 extendAgg fa rel = extend (const $ fa rel) rel
 
 
--- this represents selecting all the rows that 
---   have some aggregate value
-select :: Eq b => ([b] -> b) -> (a -> b) -> [a] -> [a]
-select chs proj rel = rfilter (\r -> proj r == aggB) rel
-  where
-    aggB = chs $ project proj rel
-
-
-select' :: (Eq a, Eq b) => ([b] -> b) -> (a -> b) -> [a] -> [a]
-select' chs proj rel = project fst $ filter (\(a, b) -> proj a == b) $ extendAgg (chs . map proj) rel 
-
-
 -- this represents selecting a single row by extreme value
 --   or something
-select1 :: (a -> a -> a) -> a -> [a] -> a
-select1 = foldl
+select :: (a -> a -> a) -> a -> [a] -> a
+select = foldl
 
 
-select1' :: (a -> a -> a) -> [a] -> Maybe a
-select1' _ [] = Nothing
-select1' f (x:xs) = Just $ foldl f x xs
+select' :: (a -> a -> a) -> [a] -> Maybe a
+select' _ [] = Nothing
+select' f (x:xs) = Just $ foldl f x xs
+
+
+-- note how 'selectMany id' specialize this to
+--    :: Eq a => (a -> a -> b) -> [a] -> [a]
+selectMany :: Eq b => (a -> b) -> (b -> b -> b) -> [a] -> [a]
+selectMany _ _ [] = []
+selectMany proj chs (r:rs) = rfilter (\r' -> proj r' == bVal) (r:rs)
+  where
+    bVal = select chs (proj r) (map proj rs) 
 
 
